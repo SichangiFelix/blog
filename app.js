5c49b93a0f3e54b3f1a616b72711b12a19252f38
SichangiFelix <felix.sichangi23@gmail.com>
2026-01-06 23:56:39 +0300

    update

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const expresslayout = require('express-ejs-layouts');
+const connectDB = require('./server/config/db')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 // assets
 app.use(express.static('public'));
